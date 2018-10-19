1a967c4dbdf38fb3e3f0183fbe4e47524fe26b3a
Andrey Astakhov <andrey.astakhov@gmail.com>
2018-10-19 11:22:27 +0200

    Change publisher to use durable queues.

--- publisher.php
+++ publisher.php
@@ -5,10 +5,11 @@ require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-$connection = new AMQPStreamConnection('rabbitmq', 5672, 'guest', 'guest');
+$connection = new AMQPStreamConnection('rabbitmq', 5672, 'guest', 'guest',
+    '/');
 $channel = $connection->channel();
 
-$channel->queue_declare('hello', false, false, false, false);
+$channel->queue_declare('hello', false, true, false, false);
 $msg = new AMQPMessage('Hello World 22!');
 
 $channel->basic_publish($msg, '', 'hello');
